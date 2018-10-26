9b65fd1ec20c2e4d91dd0d41942e6c186aa92370
xw213400 <xw213400@163.com>
2018-10-26 16:49:46 +0800

    xxx

--- cpp/src/xtesttf.cpp
+++ cpp/src/xtesttf.cpp
@@ -1,17 +1,26 @@
 
 #include "tensorflow/core/public/session.h"
+#include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/core/platform/types.h"
+#include "tensorflow/cc/ops/nn_ops.h"
 
 using namespace std;
 using namespace tensorflow;
 
+// Tensor conv5x5(int in_channel, int out_channel)
+// {
+//     ops::Conv2D conv2d(in_channel, out_channel, 5);
+// }
+
 int main(int argc, char *argv[])
 {
-    cout << "UIUIUIUI" << endl;
-    
     Session *session;
     Status status = NewSession(SessionOptions(), &session);
 
-    cout << "xxxxx" << endl;
+    Tensor a(DT_FLOAT, TensorShape({2, 2}));
+    Tensor b(DT_FLOAT, TensorShape({2, 2}));
+
+    cout << "A:" << a.DebugString() << ", B:" << b.DebugString() << endl;
 
     return 0;
 }
