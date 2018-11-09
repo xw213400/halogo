2e31d59be475d6cd648bd1f712fdc9ff77e34fc7
xiewei <xiewei-kg@gome.com.cn>
2018-11-09 15:53:45 +0800

    xxx

--- py/xtftrain.py
+++ py/xtftrain.py
@@ -38,9 +38,10 @@ def Residual_block(board, input_channel, output_channel):
     out = tf.add(conv, board)
     return tf.nn.relu(out)
 
+
 def train(positions, epoch=1):
     board = tf.placeholder(tf.float32, [None, go.N, go.N, 1])
-    labels = tf.placeholder(tf.float32, [None, go.LN+1])
+    labels = tf.placeholder(tf.float32, [None, go.LN + 1])
 
     num_planes = 32
 
@@ -53,32 +54,36 @@ def train(positions, epoch=1):
     conv5 = Residual_block(conv4, num_planes, num_planes)
     kernel2 = tf.random_normal([1, 1, num_planes, 4])
     conv6 = tf.nn.conv2d(conv5, kernel2, [1, 1, 1, 1], 'VALID')
-    linear = tf.reshape(conv6, [-1, go.LN*4])
-    weight = tf.Variable(tf.random_normal([go.LN*4, go.LN+1]))
+    linear = tf.reshape(conv6, [-1, go.LN * 4])
+    weight = tf.Variable(tf.random_normal([go.LN * 4, go.LN + 1]))
     predict = tf.matmul(linear, weight)
 
-    softmax = tf.nn.softmax(predict)
-    cross_entropy = tf.reduce_mean(-tf.reduce_sum(labels *
-                                                  tf.log(softmax+0.0000000001), reduction_indices=[1]))
+    cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(
+        labels=labels, logits=predict)
+    cross_entropy = tf.reduce_mean(cross_entropy)
+
+    # softmax = tf.nn.softmax(predict)
+    # cross_entropy = tf.reduce_mean(-tf.reduce_sum(labels *
+    #                                               tf.log(softmax+0.0000000001), reduction_indices=[1]))
     train_step = tf.train.GradientDescentOptimizer(
         0.01).minimize(cross_entropy)
-    
+
     sess = tf.Session()
     init = tf.global_variables_initializer()
     sess.run(init)
 
     for e in range(epoch):
         batch = 10
-        n = math.floor(len(positions)/batch)
+        n = math.floor(len(positions) / batch)
         i = 0
 
         random.shuffle(positions)
         while i < n:
             j = 0
             y_data = np.zeros(
-                batch*(go.LN+1), dtype=np.float32).reshape(batch, go.LN+1)
+                batch * (go.LN + 1), dtype=np.float32).reshape(batch, go.LN + 1)
             x_data = np.zeros(
-                batch*go.LN, dtype=np.float32).reshape(batch, go.N, go.N, 1)
+                batch * go.LN, dtype=np.float32).reshape(batch, go.N, go.N, 1)
             while j < batch:
                 k = i * batch + j
                 pos = positions[k]
@@ -93,7 +98,7 @@ def train(positions, epoch=1):
                 j += 1
 
             sess.run(train_step, feed_dict={labels: y_data, board: x_data})
-            print(sess.run(softmax, feed_dict={board: x_data}))
+            # print(sess.run(softmax, feed_dict={board: x_data}))
 
             i += 1
 
@@ -105,7 +110,7 @@ def main(path, epoch=1):
     positions = []
     records = [f for f in listdir(path) if f[-4:] == 'json']
     for f in records:
-        with open(path+f) as json_data:
+        with open(path + f) as json_data:
             record = json.load(json_data)
             s = 0
             parent = go.Position()
